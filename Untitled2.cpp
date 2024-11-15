ed955498abb7fb496021db6df1a66db8925895c7
Mohsin-424 <mohsinpu24@gmail.com>
2024-11-15 09:06:27 +0500

    Cpp Tutorial

---
+++ Untitled2.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream> // Include the iostream library for input and output
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl; // Output "Hello, World!" to the console
+    return 0;                                  // Indicate that the program ended successfully
+}
