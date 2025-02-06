a7e327016306db75eb0a2c561e82cd1309663a89
Mohsin-424 <mohsinpu24@gmail.com>
2025-02-06 13:17:33 +0500

    Cpp Tutorial Basics

---
+++ Mohsin.cpp
@@ -0,0 +1,80 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int number = 0;
+
+//     // if (number == 10) {
+//     //     cout << "Number is 10" << endl;
+//     // }
+//     // else {
+//     //     cout << "Number is neither 10 nor 20" << endl;
+//     // }
+
+//     // return 0;
+
+//     // for (int i = 0; i < 100; i++)
+//     // {
+//     //     cout << i << endl;
+//     // }
+//     // while (number < 10)
+//     // {
+//     //     cout << number << endl;
+//     //     number++;
+//     // }
+//     // int n1 = 10;
+//     // int n2 = 20;
+
+//     // void Alpha(int n1, int n2);
+//     // {
+//     //     cout << (n1 + n2) << endl;
+//     // }
+
+//     // return 0;
+
+//     // Arrays
+//     // int arr[] = {15, 210, 50, 9, 5};
+//     // int even = 0;
+//     // int odd = 0;
+//     // int length = sizeof(arr)/sizeof(arr[0]);
+
+//     // for (int i = 0; i < length; i++)
+//     //     {
+//     //         if (arr[i] % 2 == 0)
+//     //         {
+//     //             even++;
+//     //         }
+//     //         else
+//     //         {
+//     //             odd++;
+//     //         }
+//     //     }
+//     //     cout << "Even numbers: "  << even << endl;
+//     //     cout << "Odd numbers: " << odd << endl;
+//     //     return 0;
+
+
+
+
+
+
+
+// }
+
+#include <iostream>;
+using namespace std;
+void print_function()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        cout << "Hello World!\n";
+        i++;
+    }
+}
+int main(){
+    print_function();
+    return 0;
+ 
+}
